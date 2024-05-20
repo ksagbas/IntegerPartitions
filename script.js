e6b8160f20f546c90f35d380f69912259018d6d8
ksagbas <ksagbas@sabanciuniv.edu>
2024-05-20 17:30:30 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,187 @@
-// Counting Partitions Function
-function countPartitions(n) {
-    let dp = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
+function eulerPartitions(n) {
+    let partitions = [];
+    let p = Array(n).fill(0);
+    let k = 0;
+    p[k] = n;
 
-    // Base case: Only one way to partition 0
+    while (true) {
+        if (isValidPartition(p, k + 1, 1)) {
+            partitions.push(p.slice(0, k + 1));
+        }
+
+        let rem_val = 0;
+        while (k >= 0 && p[k] == 1) {
+            rem_val += p[k];
+            k--;
+        }
+
+        if (k < 0) return partitions;
+
+        p[k]--;
+        rem_val++;
+
+        while (rem_val > p[k]) {
+            p[k + 1] = p[k];
+            rem_val -= p[k];
+            k++;
+        }
+
+        p[k + 1] = rem_val;
+        k++;
+    }
+}
+
+function rogersRamanujanPartitions(n) {
+    let partitions = [];
+    let p = Array(n).fill(0);
+    let k = 0;
+    p[k] = n;
+
+    while (true) {
+        if (isValidPartition(p, k + 1, 2)) {
+            partitions.push(p.slice(0, k + 1));
+        }
+
+        let rem_val = 0;
+        while (k >= 0 && p[k] == 1) {
+            rem_val += p[k];
+            k--;
+        }
+
+        if (k < 0) return partitions;
+
+        p[k]--;
+        rem_val++;
+
+        while (rem_val > p[k]) {
+            p[k + 1] = p[k];
+            rem_val -= p[k];
+            k++;
+        }
+
+        p[k + 1] = rem_val;
+        k++;
+    }
+}
+
+function schurPartitions(n) {
+    let partitions = [];
+    let p = Array(n).fill(0);
+    let k = 0;
+    p[k] = n;
+
+    while (true) {
+        if (isValidPartitionSchur(p, k + 1)) {
+            partitions.push(p.slice(0, k + 1));
+        }
+
+        let rem_val = 0;
+        while (k >= 0 && p[k] == 1) {
+            rem_val += p[k];
+            k--;
+        }
+
+        if (k < 0) return partitions;
+
+        p[k]--;
+        rem_val++;
+
+        while (rem_val > p[k]) {
+            p[k + 1] = p[k];
+            rem_val -= p[k];
+            k++;
+        }
+
+        p[k + 1] = rem_val;
+        k++;
+    }
+}
+
+function goellnitzGordonPartitions(n) {
+    let partitions = [];
+    let p = Array(n).fill(0);
+    let k = 0;
+    p[k] = n;
+
+    while (true) {
+        if (isValidPartitionGoellnitzGordon(p, k + 1)) {
+            partitions.push(p.slice(0, k + 1));
+        }
+
+        let rem_val = 0;
+        while (k >= 0 && p[k] == 1) {
+            rem_val += p[k];
+            k--;
+        }
+
+        if (k < 0) return partitions;
+
+        p[k]--;
+        rem_val++;
+
+        while (rem_val > p[k]) {
+            p[k + 1] = p[k];
+            rem_val -= p[k];
+            k++;
+        }
+
+        p[k + 1] = rem_val;
+        k++;
+    }
+}
+
+function isValidPartition(p, n, diff) {
+    for (let i = 1; i < n; i++) {
+        if (p[i - 1] - p[i] < diff) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function isValidPartitionSchur(p, n) {
+    for (let i = 1; i < n; i++) {
+        if ((p[i - 1] - p[i] < 3) || (p[i - 1] % 3 == 0 && p[i] % 3 == 0 && p[i - 1] - p[i] == 3)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function isValidPartitionGoellnitzGordon(p, n) {
+    for (let i = 0; i < n; i++) {
+        if (p[i] < 3) {
+            return false;
+        }
+        if ((i > 0 && p[i - 1] - p[i] < 2) || (i > 0 && p[i] % 2 == 0 && p[i - 1] % 2 == 0)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function displayGenerating() {
+    const numberInput = document.getElementById('numberInputGen').value;
+    let partitions;
+    const generatingTitle = document.getElementById('generatingTitle').innerText;
+
+    if (generatingTitle.includes("Euler Identity")) {
+        partitions = eulerPartitions(Number(numberInput));
+    } else if (generatingTitle.includes("Rogers-Ramanujan Identity")) {
+        partitions = rogersRamanujanPartitions(Number(numberInput));
+    } else if (generatingTitle.includes("Schur Identity")) {
+        partitions = schurPartitions(Number(numberInput));
+    } else if (generatingTitle.includes("2nd Göllnitz-Gordon Identity")) {
+        partitions = goellnitzGordonPartitions(Number(numberInput));
+    }
+
+    document.getElementById('resultGen').innerText = `Integer Partitions:\n${partitions.map(partition => partition.join(' + ')).join('\n')}`;
+}
+
+function eulerCountingPartitions(n) {
+    let dp = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
     dp[0][0] = 1;
 
-    // Calculate the partitions iteratively
     for (let i = 1; i <= n; ++i) {
         for (let k = 0; k <= n; ++k) {
             if (i <= k) {
@@ -16,97 +192,128 @@ function countPartitions(n) {
         }
     }
 
-    // The answer is stored in dp[n][n]
     return dp[n][n];
 }
 
-function displayPartitions() {
-    const numberInput = document.getElementById('numberInput').value;
-    const result = countPartitions(Number(numberInput));
-    document.getElementById('result').innerText = `Number of partitions with the Euler's rule: ${result}`;
-}
+function rogersRamanujanCountingPartitions(n) {
+    let dp = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
+    dp[0][0] = 1;
 
-// Generating Partitions Function
-function printArray(p, n) {
-    let result = '';
-    for (let i = 0; i < n; i++) {
-        result += p[i];
-        if (i < n - 1) {
-            result += ' + ';
+    for (let i = 1; i <= n; ++i) {
+        for (let k = 0; k <= n; ++k) {
+            if (i <= k && (i - 2 >= 0)) {
+                dp[i][k] = dp[i - 2][k - i] + dp[i - 1][k];
+            } else if (i - 2 < 0 && i <= k) {
+                dp[i][k] = dp[0][k - i] + dp[i - 1][k];
+            } else {
+                dp[i][k] = dp[i - 1][k];
+            }
         }
     }
-    return result;
+
+    return dp[n][n];
 }
 
-function isValidPartition(p, n) {
-    for (let i = 1; i < n; i++) {
-        if (p[i - 1] - p[i] < 1) {
-            return false;
+function schurCountingPartitions(n) {
+    let dp = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
+    dp[0][0] = 1;
+
+    for (let i = 1; i <= n; ++i) {
+        let a = (i % 3 === 0) ? 4 : 3;
+
+        for (let k = 0; k <= n; ++k) {
+            if (i <= k && (i - a >= 0)) {
+                dp[i][k] = dp[i - a][k - i] + dp[i - 1][k];
+            } else if (i - a < 0 && i <= k) {
+                dp[i][k] = dp[0][k - i] + dp[i - 1][k];
+            } else {
+                dp[i][k] = dp[i - 1][k];
+            }
         }
     }
-    return true;
+
+    return dp[n][n];
 }
 
-function printAllUniqueParts(n) {
-    let p = Array(n).fill(0); // A vector to store a partition 
-    let k = 0; // Index of last element in a partition 
-    p[k] = n; // Initialize first partition as number itself 
-    let result = [];
-
-    // This loop first prints current partition then generates next 
-    // partition. The loop stops when the current partition has all 1s 
-    while (true) {
-        // Check if the current partition meets the condition
-        if (isValidPartition(p, k + 1)) {
-            // Add current partition to result
-            result.push(printArray(p, k + 1));
-        }
-
-        // Generate next partition 
-        let rem_val = 0;
-        while (k >= 0 && p[k] == 1) {
-            rem_val += p[k];
-            k--;
-        }
-
-        // if k < 0, all the values are 1 so there are no more partitions 
-        if (k < 0) return result;
-
-        // Decrease the p[k] found above and adjust the rem_val 
-        p[k]--;
-        rem_val++;
-
-        // If rem_val is more, then the sorted order is violated. Divide 
-        // rem_val in different values of size p[k] and copy these values at 
-        // different positions after p[k] 
-        while (rem_val > p[k]) {
-            p[k + 1] = p[k];
-            rem_val = rem_val - p[k];
-            k++;
-        }
-
-        // Copy rem_val to next position and increment position 
-        p[k + 1] = rem_val;
-        k++;
+function goellnitzGordonCountingPartitions(n) {
+    let dp = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
+    for (let c = 0; c <= 2; c++) {
+        dp[c][0] = 1;
     }
+
+    for (let i = 3; i <= n; ++i) {
+        let a = (i % 2 === 0) ? 3 : 2;
+
+        for (let k = 0; k <= n; ++k) {
+            if (i <= k && (i - a >= 0)) {
+                dp[i][k] = dp[i - a][k - i] + dp[i - 1][k];
+            } else if (i - a < 0 && i <= k) {
+                dp[i][k] = dp[0][k - i] + dp[i - 1][k];
+            } else {
+                dp[i][k] = dp[i - 1][k];
+            }
+        }
+    }
+
+    return dp[n][n];
 }
 
-function displayGenerating() {
-    const numberInput = document.getElementById('numberInputGen').value;
-    const partitions = printAllUniqueParts(Number(numberInput));
-    document.getElementById('resultGen').innerText = `All Unique Partitions of ${numberInput}:\n${partitions.join('\n')}`;
+function displayCounting() {
+    const numberInput = document.getElementById('numberInputCount').value;
+    let result;
+    const countingTitle = document.getElementById('countingTitle').innerText;
+
+    if (countingTitle.includes("Euler Identity")) {
+        result = eulerCountingPartitions(Number(numberInput));
+    } else if (countingTitle.includes("Rogers-Ramanujan Identity")) {
+        result = rogersRamanujanCountingPartitions(Number(numberInput));
+    } else if (countingTitle.includes("Schur Identity")) {
+        result = schurCountingPartitions(Number(numberInput));
+    } else if (countingTitle.includes("2nd Göllnitz-Gordon Identity")) {
+        result = goellnitzGordonCountingPartitions(Number(numberInput));
+    }
+
+    document.getElementById('resultCount').innerText = `Number of partitions:\n${result}`;
 }
 
-// Functions to toggle between content
-function showGenerating() {
+function showGenerating(type) {
+    clearInputsAndResults();
     document.getElementById('generatingContent').style.display = 'block';
     document.getElementById('countingContent').style.display = 'none';
+
+    if (type === 'Euler') {
+        document.getElementById('generatingTitle').innerText = 'Generating Partitions - Euler Identity';
+    } else if (type === 'RogersRamanujan') {
+        document.getElementById('generatingTitle').innerText = 'Generating Partitions - Rogers-Ramanujan Identity';
+    } else if (type === 'Schur') {
+        document.getElementById('generatingTitle').innerText = 'Generating Partitions - Schur Identity';
+    } else if (type === 'GoellnitzGordon') {
+        document.getElementById('generatingTitle').innerText = 'Generating Partitions - 2nd Göllnitz-Gordon Identity';
+    }
 }
 
-function showCounting() {
+function showCounting(type) {
+    clearInputsAndResults();
     document.getElementById('generatingContent').style.display = 'none';
     document.getElementById('countingContent').style.display = 'block';
+
+    if (type === 'Euler') {
+        document.getElementById('countingTitle').innerText = 'Counting Integer Partitions - Euler Identity';
+    } else if (type === 'RogersRamanujan') {
+        document.getElementById('countingTitle').innerText = 'Counting Integer Partitions - Rogers-Ramanujan Identity';
+    } else if (type === 'Schur') {
+        document.getElementById('countingTitle').innerText = 'Counting Integer Partitions - Schur Identity';
+    } else if (type === 'GoellnitzGordon') {
+        document.getElementById('countingTitle').innerText = 'Counting Integer Partitions - 2nd Göllnitz-Gordon Identity';
+    }
+}
+
+function clearInputsAndResults() {
+    document.getElementById('numberInputGen').value = '';
+    document.getElementById('resultGen').innerText = '';
+    document.getElementById('numberInputCount').value = '';
+    document.getElementById('resultCount').innerText = '';
 }
 
 // Show the generating content by default
-showGenerating();
+showGenerating('Euler');
